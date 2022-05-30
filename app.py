4eff8ce7412e66ba0bdb3066611766b2273e0702
mazqoty <mazqoty.01@gmail.com>
2022-05-30 13:49:12 +0300

    Finished Product

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ print("Twitter API connected")
 app = dash.Dash(__name__, suppress_callback_exceptions = True, external_stylesheets = [dbc.themes.LUX], 
                 meta_tags = [{'name': 'viewport', 'content': 'width = device-width, initial-scale = 1.0'}]
                 )
-
+app.title = "Twitter Analytics Dash App"
 server = app.server
 
+# The way gunicorn works import following to avoid circular imports
 import appTabs
