b4b5cb7e240be597a2371f9553bbf7721d647372
Mazhar <34889453+mazqoty@users.noreply.github.com>
2022-05-30 14:46:12 +0300

    Update appTabs.py

--- appTabs.py
+++ appTabs.py
@@ -60,5 +60,8 @@ def switch_tab(tab_chosen):
 
 
 if __name__ == '__main__':
-    app.run_server(debug = True, port = 8050)
+    # for local
+    #app.run_server(debug = True, port = 8050)
+    #for docker
+    app.run_server()
     
